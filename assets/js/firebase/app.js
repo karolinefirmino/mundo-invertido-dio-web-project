408ebf48f1e01ae3c33b5b04999624017ed70f66
Karoline Firmino <krodrigues.firmino2@gmail.com>
2022-09-06 13:50:05 -0300

    atualizacao-firebase

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA36E0HlZi8lJvHJziZCo3sYTPgd-xEvrg",
+    authDomain: "semana-mundo-invertido-8cd3e.firebaseapp.com",
+    projectId: "semana-mundo-invertido-8cd3e",
+    storageBucket: "semana-mundo-invertido-8cd3e.appspot.com",
+    messagingSenderId: "826922986186",
+    appId: "1:826922986186:web:0908c72890768319116233"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export default app
---
+++ assets/js/firebase/hellfire-club.js
@@ -0,0 +1,7 @@
+import app from './firebase/app.js'
+import { } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js'
+
+export function subscribeToHellfireClub(subscription) {
+    console.log('Subscription', subscription)
+
+}
